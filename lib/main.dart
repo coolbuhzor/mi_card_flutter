e530c783673c8b4443afb3c0d51ec1bcaa2e16b6
coolbuhzor <coolbuhzor@gmail.com>
2023-05-04 23:01:40 +0100

    app init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(50, 10, 20, 5),
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
